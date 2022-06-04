a7b73f1d2dbb394fe82fa6e8835e61152eea30a2
Juan8ocho <101667814+Juan8ocho@users.noreply.github.com>
2022-06-04 14:57:13 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,44 @@
+function modonoche() {
+  document.getElementById("cuerpo").style.backgroundColor = "black";
+  document.getElementById("cuerpo").style.color= 'white';
+  document.getElementById("cabeza").style.backgroundColor = "black";
+  document.getElementById("cabeza").style.color= 'white';
+  document.getElementById("intro").style.backgroundColor = "black";
+  document.getElementById("intro").style.color= 'white';
+  document.getElementById("titulo").style.backgroundColor = "black";
+  document.getElementById("titulo").style.color= 'white';
+  document.getElementById("nav").style.backgroundColor = "black";
+  document.getElementById("nav").style.color= 'white';
+  document.getElementById("colum").style.backgroundColor = "black";
+  document.getElementById("colum").style.color= 'white';
+  document.getElementById("principal").style.backgroundColor = "black";
+  document.getElementById("principal").style.color= 'white';
+  document.getElementById("parra1").style.backgroundColor = "black";
+  document.getElementById("parra1").style.color= 'white';
+  document.getElementById("torre1").style.backgroundColor = "black";
+  document.getElementById("torre1").style.color= 'white';
+  document.getElementById("torre2").style.backgroundColor = "black";
+  document.getElementById("torre2").style.color= 'white';
+  document.getElementById("torre3").style.backgroundColor = "black";
+  document.getElementById("torre3").style.color= 'white';
+  document.getElementById("torre4").style.backgroundColor = "black";
+  document.getElementById("torre4").style.color= 'white';
+  document.getElementById("torre5").style.backgroundColor = "black";
+  document.getElementById("torre5").style.color= 'white';
+  document.getElementById("asi1").style.backgroundColor = "black";
+  document.getElementById("asi1").style.color= 'white';
+  document.getElementById("foo").style.backgroundColor = "black";
+  document.getElementById("foo").style.color= 'white';
+  document.getElementById("cartitas1").style.backgroundColor = "black";
+  document.getElementById("cartitas1").style.color= 'white';
+  document.getElementById("cartitas2").style.backgroundColor = "black";
+  document.getElementById("cartitas2").style.color= 'white';
+  document.getElementById("cartitas3").style.backgroundColor = "black";
+  document.getElementById("cartitas3").style.color= 'white';
+  document.getElementById("cartitas4").style.backgroundColor = "black";
+  document.getElementById("cartitas4").style.color= 'white';
+  document.getElementById("redes").style.backgroundColor = "black";
+  document.getElementById("redes").style.color= 'white';
+
+
+}
